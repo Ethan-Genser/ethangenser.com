06959dea2547924b957d9e33a0523d92fb015865
Ethan Genser <ethanp1033@gmail.com>
2022-04-27 00:17:20 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const PORT = 3000;
+
+// Node dependencies
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const bodyParser = require('body-parser');
+const res = require('express/lib/response');
+const path = require('path');
+const { runInNewContext } = require('vm');
+const { send } = require('process');
+
+// ----- Express http server -----
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json())
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+// Views
+app.get('/', (req, res) => {
+    console.log(`http request received: type: GET, path: '/', from: ${req.socket.remoteAddress}`);
+    res.render('pages/index');
+});
+// Binds server to the port and stars listening.
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+});
